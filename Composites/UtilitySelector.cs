81d4dde80e19d3c292881d1de23a8f40b4ef56c1
Huawei Wang <huawei_007@yahoo.com>
2019-08-18 08:18:14 +0800

    add utility selector.

---
+++ Composites/UtilitySelector.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BetterDriver.Composites
+{
+    class UtilitySelector : Selector
+    {
+        public override void Enter()
+        {
+            Clear();
+            Children.Sort((x, y) => (int)(x.Utility - y.Utility));
+            var child = Children[CurrentIndex];
+            child.Enter();
+        }
+    }
+}
